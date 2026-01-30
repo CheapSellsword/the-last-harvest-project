3b39b388ba44d9e9ed465141ebedb5b3441a229d
CheapSellsword <almuchwork@gmail.com>
2026-01-30 18:34:04 +0300

    fix hoe tres 1

--- data/definitions/tool_item_definiton.gd
+++ data/definitions/tool_item_definiton.gd
@@ -1,6 +1,19 @@
+# tool_item_definition.gd
+# This extends the base ItemDefinition to add tool-specific data.
 @tool
 class_name ToolItemDefinition
 extends ItemDefinition
 
-@export_enum("hoe", "pickaxe", "axe", "watering_can") var tool_type: String = "hoe"
-@export var stamina_cost: int = 2
+enum ToolType {
+	HOE,
+	PICKAXE,
+	AXE,
+	WATERING_CAN,
+	SWORD
+}
+
+## What kind of tool is this? Used by the system to determine behavior (tilling, mining, etc).
+@export var tool_type: ToolType = ToolType.HOE
+
+## (Optional) How much stamina using this tool consumes.
+@export var stamina_cost: float = 2.0
